ec12f80f7be4a9df4ff346d8154f99a6db38f416
Gregory Priznar <44954435+Gpriznar@users.noreply.github.com>
2019-03-20 15:32:11 -0500

    Wednesday Final Commits

--- flashCODE/app.js
+++ flashCODE/app.js
@@ -1,19 +1,58 @@
-// Python
+let cardDef = document.getElementById("cardDef")
+let cardTerm = document.getElementById('cardTerm')
+let nextButton = document.getElementById('nextButton')
+let previousButton = document.getElementById('previousButton')
+let language = document.getElementById('language').value
 
-let pythondef = document.getElementById("pythondef")
-let pythonanswer = document.getElementById('pythonanswer')
-let nextbutton = document.getElementById('nextbutton')
+let languageObject = {
+  python: pythonFlashCards,
+  javascript: javascriptFlashCards,
+  html: htmlFlashCards,
+  commandLine: commandlineFlashCards,
+  css: cssFlashCards
+}
 
-
-let database = firebase.database()
+let cardArray = languageObject[language]
 
 let counter = 0
 
-nextbutton.addEventListener('click',function(){
 
-let questionItem = `<p> ${pythonflashcards[counter].Term}</p>`
-let solutionItem = `<p>${pythonflashcards[counter].Definition}</p>`
-counter += 1
-pythondef.innerHTML = questionItem
-pythonanswer.innerHTML = solutionItem
-})
+cardDef.innerHTML = cardArray[counter].Term
+cardTerm.innerHTML = cardArray[counter].Definition
+previousButton.style.visibility = 'hidden'
+
+function buttonDisplay(counter) {
+  if(counter <= 0) {
+    previousButton.style.visibility = 'hidden'
+  }
+  else if (counter >= cardArray.length - 1) {
+    nextButton.style.visibility = 'hidden'
+  }
+  else {
+    previousButton.style.visibility = 'visible'
+    nextButton.style.visibility = 'visible'
+  }
+}
+
+nextButton.addEventListener('click',function(){
+  counter += 1
+
+  let questionItem = `<p>${cardArray[counter].Term}</p>`
+  let solutionItem = `<p>${cardArray[counter].Definition}</p>`
+  cardDef.innerHTML = solutionItem
+  cardTerm.innerHTML = questionItem
+
+  buttonDisplay(counter)
+  })
+
+previousButton.addEventListener('click',function(){
+  counter -= 1
+
+  let questionItem = `<p>${cardArray[counter].Term}</p>`
+  let solutionItem = `<p>${cardArray[counter].Definition}</p>`
+  cardDef.innerHTML = solutionItem
+  cardTerm.innerHTML = questionItem
+
+  buttonDisplay(counter)
+
+  })
